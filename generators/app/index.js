0ed3c278fad9d19b8cbeaa91c768e638130a1292
Siddharth Srivastava <vish.sid73@gmail.com>
2017-07-10 16:08:51 +0530

    added the gitignore file

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,21 +10,27 @@ module.exports = class extends Generator {
       "Welcome to the Sid's " + chalk.red('Express-API-Stub') + " generator!"
     ));
 
-    //const prompts = [{
-    //  type: 'confirm',
-    //  name: 'someAnswer',
-    //  message: 'Would you like to enable this option?',
-    //  default: true
-    //}];
-    //
-    //return this.prompt(prompts).then(props => {
-    //  // To access props later use this.props.someAnswer;
-    //  this.props = props;
-    //});
+    const prompts = [{
+      type: 'input',
+      name: 'name',
+      message: 'Your project name',
+      default: this.appname
+    }];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
   }
 
   writing() {
-    this.fs.copy(
+
+  this.fs.copyTpl(
+      this.templatePath('_package.json'),
+      this.destinationPath('package.json'),
+      { title: this.props.name }
+  );
+  this.fs.copy(
       this.templatePath(),
       this.destinationPath()
     );
